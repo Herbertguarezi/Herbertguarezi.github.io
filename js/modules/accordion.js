dd2c6fe1b382e42c805b686c8cfffe7bea532186
Herbert Guarezi <herbertguarezi@gmail.com>
2024-02-05 15:20:48 -0300

    refatorado accordion e corrigido scroll

--- js/modules/accordion.js
+++ js/modules/accordion.js
@@ -1,18 +1,28 @@
-export default function initAccordion() {
-  const accordionList = document.querySelectorAll(
-    '[data-anime="accordion"] dt'
-  );
-  const activeClass = "ativo";
-  function activeAccordion() {
-    this.classList.toggle(activeClass);
-    this.nextElementSibling.classList.toggle(activeClass);
+export default class Accordion {
+  constructor(selection) {
+    this.accordionList = document.querySelectorAll(selection);
+    this.activeClass = "ativo";
   }
 
-  if (accordionList.length) {
-    accordionList[0].classList.add(activeClass);
-    accordionList[0].nextElementSibling.classList.add(activeClass);
-    accordionList.forEach((item) => {
-      item.addEventListener("click", activeAccordion);
+  // ativa ou desativa o accordion
+  toggleAccordion(item) {
+    item.classList.toggle(this.activeClass);
+    item.nextElementSibling.classList.toggle(this.activeClass);
+  }
+
+  // adiciona os eventos ao accordion
+  addAccordionEvent() {
+    this.accordionList.forEach((item) => {
+      item.addEventListener("click", () => this.toggleAccordion(item));
     });
+  }
+
+  // iniciar função
+  init() {
+    if (this.accordionList.length) {
+      // ativar primeiro item
+      this.toggleAccordion(this.accordionList[0]);
+      this.addAccordionEvent();
+    }
   }
 }
