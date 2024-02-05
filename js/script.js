1616ea93e3abd2edf9c7d4f056ed4ca51e586800
Herbert Guarezi <herbertguarezi@gmail.com>
2024-02-05 16:12:16 -0300

    refatorado a tab-nav

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 import ScrollSuave from "./modules/scroll-suave.js";
 import initAnimacaoScroll from "./modules/scroll-animacao.js";
 import Accordion from "./modules/accordion.js";
-import initTabNav from "./modules/tab-menu.js";
+import TabNav from "./modules/tab-menu.js";
 import initModal from "./modules/modal.js";
 import initTolltip from "./modules/tooltip.js";
 import initDropdownMenu from "./modules/dropdown-menu.js";
@@ -15,7 +15,13 @@ scrollSuave.init();
 
 const accordion = new Accordion('[data-anime="accordion"] dt');
 accordion.init();
-initTabNav();
+
+const tabNav = new TabNav(
+  '[data-tab="menu"] li',
+  '[data-tab="content"] section'
+);
+tabNav.init();
+
 initModal();
 initTolltip();
 initDropdownMenu();
